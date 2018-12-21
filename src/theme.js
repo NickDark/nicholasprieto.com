b0016761758b09d35ffe67467e5335d000b23d1e
Nicholas Prieto <nicholasmprieto@gmail.com>
2018-12-20 21:52:56 -0500

    First commit - new website based on Gatsby Mate starter.

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+const colors = {
+  background: '#ffffff',
+  backgroundDark: '#ffffff',
+
+  primary: '#000000',
+  primaryLight: '#000000',
+  primaryDark: '#000000',
+
+  secondary: '#a7fe77',
+  secondaryLight: '#ffffff',
+  secondaryDark: '#000000',
+};
+
+module.exports = {
+  colors,
+};
+// #a7fe77
